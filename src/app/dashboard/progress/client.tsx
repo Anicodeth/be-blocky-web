a3167b26c689bc939566731623d2234cbb07e069
Mikiyas Tibebu <mikitebe123@gmail.com>
2023-09-18 19:12:34 +0300

    added the child tab

--- src/app/dashboard/progress/client.tsx
+++ src/app/dashboard/progress/client.tsx
@@ -1,22 +1,58 @@
-"use client"
-import { Tab, Tabs } from "@nextui-org/react"
-import { tabStyle } from '../../../lib/style';
+"use client";
+import { Tab, Tabs } from "@nextui-org/react";
+import { tabStyle } from "../../../lib/style";
+import { Classroom, Student } from "@/types";
+import { AddChildModal } from "@/components/dialogs/add-child-modal";
+import { EmptyPlaceholder } from "@/components/empty-placehoder";
+import { StudentCard } from "@/components/student-card";
 
-
-
-export const ProgressTabs = () => {
-    return (
-        <Tabs aria-label="Options" size="lg" variant="solid" radius="full" classNames={{
-            tabList: "bg-brand-green rounded-full w-1/2",
-            tabContent: "group-data-[selected=true]:text-[#68b946] text-[#ffffff] font-bold md:text-xl px-4",
-            base: "flex flex-col items-center"
-        }}>
-            <Tab key="child" title="Child" className=" flex md:items-center flex-col md:flex-row">
-
-            </Tab>
-            <Tab key="children" title="Children">
-
-            </Tab>
-        </Tabs>
-    )
+interface props {
+  data:
+    | {
+        student: Student[];
+        role: "parent";
+      }
+    | {
+        role: "school";
+        student?: undefined;
+      };
+  school:
+    | {
+        classRoom: Classroom;
+        students: Student[];
+      }[]
+    | null;
 }
+export const ProgressTabs = ({ data, school }: props) => {
+  return (
+    <Tabs
+      aria-label="Options"
+      size="lg"
+      variant="solid"
+      radius="full"
+      classNames={{
+        tabList: "bg-brand-green rounded-full w-1/2",
+        tabContent:
+          "group-data-[selected=true]:text-[#68b946] text-[#ffffff] font-bold md:text-xl px-4",
+        base: "flex flex-col items-center",
+      }}
+    >
+      <Tab key="child" title="Child">
+        {data.student?.length ? (
+          <div>
+            {data.student.map((student) => (
+              <StudentCard student={student} key={student.name} />
+            ))}
+          </div>
+        ) : (
+          <EmptyPlaceholder>
+            <EmptyPlaceholder.Icon name="Student" />
+            <EmptyPlaceholder.Title>No Children/Student Added</EmptyPlaceholder.Title>
+            <AddChildModal />
+          </EmptyPlaceholder>
+        )}
+      </Tab>
+      <Tab key="children" title="Children"></Tab>
+    </Tabs>
+  );
+};
--- src/app/dashboard/progress/page.tsx
+++ src/app/dashboard/progress/page.tsx
@@ -1,13 +1,17 @@
 import { PageHeader } from "@/components/page-header";
 import { ProgressTabs } from "./client";
+import { getDashboardData } from "@/actions/parents";
+import { getSchools } from "@/actions/schools";
 
 
-export default function page() {
+export default async function page() {
+    const data = await getDashboardData();
+    const school = await getSchools()
     return (
         <div>
             <PageHeader />
             <div className=" my-4">
-                <ProgressTabs />
+                <ProgressTabs data={data} school={school} />
             </div>
         </div>
     )
