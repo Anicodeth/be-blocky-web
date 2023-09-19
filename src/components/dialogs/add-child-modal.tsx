e6d369cc349141d89d1e7d860440efd56d379b1a
bekacru <bekacru@gmail.com>
2023-09-19 08:57:56 +0300

    child modal

--- src/components/dialogs/add-child-modal.tsx
+++ src/components/dialogs/add-child-modal.tsx
@@ -40,6 +40,7 @@ export function AddChildModal({ another, isSchool, classrooms }: Props) {
     const isMobile = useIsMobile()
     const [accountType, setAccountType] = useState<"new" | "existing">()
     const name = isSchool ? "Student" : "Child"
+    
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
